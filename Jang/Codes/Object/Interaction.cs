79e208477e2fc00a37dee9a717cf0520383845fc
rainbootsjeans <154078192+rainbootsjeans@users.noreply.github.com>
2024-06-10 13:02:12 +0900

    interaction is so suck

--- Jang/Codes/Object/Interaction.cs
+++ Jang/Codes/Object/Interaction.cs
@@ -9,14 +9,14 @@ using UnityEngine;
 // 기본적으로 상호작용 가능한 오브젝트의 첫번째 자식오브젝트는 상호작용키여야함
 // 매우 중요 :
 // 이 스크립트를 쓰는 오브젝트에는 다음 항목들이 필수적으로 들어가야함
-// 상호작용 했을 때 실행될 interaction이 있는 스크립트를 인스펙터에서 할당
-// 해당 스크립트 class 선언에서,  … MonoBehaviour, IInteractable 처럼 선언되어야 함
+// 상호작용 했을 때 실행될 interaction()이 있는 스크립트를 인스펙터에서 할당
+
 public class Interaction : MonoBehaviour
 {
     Rigidbody2D rigid;
     SpriteRenderer sprite;
     Animator anim;
-    Transform playerTarget; // 지금 플레이어의 상호작용 타겟
+    PlayerInteraction playerTarget; // 지금 플레이어의 상호작용 타겟
     public MonoBehaviour interactableScript; // 상호작용 했을 때, 실행될 interaction()이 있는 스크립트
 
     void Awake()
@@ -24,7 +24,7 @@ public class Interaction : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        playerTarget = GetComponent<PlayerInteraction>().result;
+        playerTarget = GetComponent<PlayerInteraction>();
     }
     public interface IInteractable
     {
@@ -41,7 +41,7 @@ public class Interaction : MonoBehaviour
     }
     public void showInteractionKey()
     {
-        if (playerTarget == this.transform)
+        if (playerTarget.result == this.transform)
         {
             this.transform.GetChild(0).gameObject.SetActive(true);
         }
@@ -49,5 +49,20 @@ public class Interaction : MonoBehaviour
         {
             this.transform.GetChild(0).gameObject.SetActive(false);
         }
+    }
+
+    void testInteraction()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            Debug.Log("씨발");
+            IInteractable interactable = interactableScript as IInteractable;
+            interactable.interaction();
+        }
+    }
+
+    void Update()
+    {
+        testInteraction();
     }
 }
